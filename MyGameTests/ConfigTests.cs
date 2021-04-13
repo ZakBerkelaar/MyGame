a2d418f6769c4f996f4d91586716e56e3a47f136
MrSlimeDude <45675291+MrSlimeDude@users.noreply.github.com>
2021-04-13 16:31:05 -0400

    Added config tests

---
+++ MyGameTests/ConfigTests.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyGame;
+
+namespace MyGameTests
+{
+    [TestClass]
+    public class ConfigTests
+    {
+        private Config config;
+
+        [TestInitialize]
+        public void ConfigTestInit()
+        {
+            config = new Config(new IDString("Testing", "Config", "Test"));
+        }
+
+        [TestMethod]
+        public void TestString()
+        {
+            const string test = "";
+            config.Write("TestString", test);
+            Assert.AreEqual(test, config.Read<string>("TestString"));
+        }
+
+        [TestMethod]
+        public void TestInt()
+        {
+            const int test = 21;
+            config.Write("TestInt", test);
+            Assert.AreEqual(test, config.Read<int>("TestInt"));
+        }
+        
+
+        [TestMethod]
+        public void TestFloat()
+        {
+            const float test = 6.9f;
+            config.Write("TestFloat", test);
+            Assert.AreEqual(test, config.Read<float>("TestFloat"));
+        }
+
+        [TestMethod]
+        public void TestDouble()
+        {
+            const double test = 4.2d;
+            config.Write("TestDouble", test);
+            Assert.AreEqual(test, config.Read<double>("TestDouble"));
+        }
+
+        [TestMethod]
+        public void TestDecimal()
+        {
+            const decimal test = 12.1m;
+            config.Write("TestDecimal", test);
+            Assert.AreEqual(test, config.Read<decimal>("TestDecimal"));
+        }
+
+        [TestMethod]
+        public void TestDuplicateKey()
+        {
+            config.Write("TestDupe", 0);
+            config.Write("TestDupe", 5);
+            Assert.AreEqual(5, config.Read<int>("TestDupe"));
+        }
+    }
+}

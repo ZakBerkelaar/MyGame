db9a41cd47879adf4c679be0792603aff3d68a78
MrSlimeDude <45675291+MrSlimeDude@users.noreply.github.com>
2022-05-23 18:59:49 -0400

    Added a benchmarking project

---
+++ MyGameBenchmarks/SpanTesting.cs
@@ -0,0 +1,34 @@
+﻿using BenchmarkDotNet.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyGameBenchmarks
+{
+    public class SpanTesting
+    {
+        private Random random;
+        public SpanTesting()
+        {
+            random = new Random();
+        }
+
+        [Benchmark]
+        public void Array()
+        {
+            float[] arr = new float[30];
+            for (int i = 0; i < arr.Length; i++)
+                arr[i] = (float)random.NextDouble();
+        }
+
+        [Benchmark]
+        public void Span()
+        {
+            Span<float> arr = stackalloc float[30];
+            for (int i = 0; i < arr.Length; i++)
+                arr[i] = (float)random.NextDouble();
+        }
+    }
+}
